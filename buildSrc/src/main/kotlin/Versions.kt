f7d80c5d5077fcb35bcf2b3a5f3ca04b9b27046f
Faraz <mfarazrasheed92@gmail.com>
2021-07-26 23:29:15 +0500

    main code base

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,36 @@
+/**
+ * Created by Faraz Rasheed on 26/07/2021.
+ */
+object Versions {
+    // Gradle Plugin
+    const val classpathGradleVersion = "4.2.2"
+
+    // Core
+    const val kotlinVersion = "1.3.50"
+    const val appCompatVersion = "1.2.0"
+    const val coreKtxVersion = "1.3.2"
+
+    // Design
+    const val constraintLayoutVersion = "2.0.4"
+    const val materialVersion = "1.3.0"
+
+    // Test
+    const val junitVersion = "4.13.2"
+    const val junitTestVersion = "1.1.3"
+    const val espressoCoreVersion = "3.4.0"
+
+    //External Lib
+    const val calendarLib = "1.0.7"
+
+
+    const val playServicesVersion = "16.0.0"
+    const val supportLibraryVersion = "28.0.3"
+    const val retrofitVersion = "2.6.0"
+    const val daggerVersion = "2.16"
+    const val okhttpVersion = "3.12.1"
+    const val okhttpLoggerVersion = "3.10.0"
+    const val okhttpGsonVersion = "2.5.0"
+    const val arch = "2.1.0"
+    const val paging_version = "2.1.0"
+
+}
